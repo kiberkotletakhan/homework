8712909d9514b11de996c32a4b1361e9d3223887
kiberkotletaklan <69646724+kiberkotletaklan@users.noreply.github.com>
2020-12-12 16:10:16 +0200

    Update homework.rb

--- homework.rb
+++ homework.rb
@@ -1,24 +1,7 @@
 class Employees
 attr_accessor :name
 
-
-  def self.menu
-    puts '1. If you want to add a new employee.'
-    puts '2. If you want to exit.'
-    print '>'
-    input = $stdin.gets.chomp
-    add_new_employee if input.to_i == 1
-  end
-
-  def self.add_new_employee
-    puts 'please insert employee name'
-    name = gets.chomp
-    employee = employee.new(name)
-    employee.change
-    puts '-----------------'
-    menu
-  end
-
+  
   def initialize(name)
     @name = name
   end
@@ -52,8 +35,4 @@ until answer == "no"
   employee.change
   puts "do you wish to add a new employee"
   answer = gets.chomp
-end
-
-employee.each do |worker|
-  puts worker
 end
